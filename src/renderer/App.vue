39c3b7b48e48d20afe2f82d4434af003602446f4
Miguel González <migueljgonz@gmail.com>
2019-06-25 20:46:59 +0200

    Load main stylus in App

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="root">
     <router-view></router-view>
   </div>
 </template>
@@ -10,6 +10,6 @@
   };
 </script>
 
-<style>
-  /* CSS */
+<style lang="stylus">
+@import './style/main'
 </style>
