d2fa87af23781ece33295ac680f3a4c8d7efd2e2
Miguel González <migueljgonz@gmail.com>
2019-06-25 21:15:17 +0200

    Apply the appropriate vuetify layout

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <div id="root">
-    <router-view></router-view>
-  </div>
+  <v-app id="root">
+    <v-navigation-drawer app></v-navigation-drawer>
+    <v-toolbar app></v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <script>
