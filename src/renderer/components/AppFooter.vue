870a2fcad164b425669c4c71aedd60acfa1d6e32
Miguel González <migueljgonz@gmail.com>
2019-06-27 23:03:03 +0200

    Open links in the external browser

--- src/renderer/components/AppFooter.vue
+++ src/renderer/components/AppFooter.vue
@@ -2,8 +2,12 @@
   <v-footer app height="auto" color="primary lighten-3">
     <v-layout justify-center row wrap>
       <v-flex py-3 text-xs-center white--text xs12>
-        <a href="https://github.com/mig42">mig42</a> –
-        <a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU GPLv3</a>
+        <external-link href="https://github.com/mig42">
+          mig42
+        </external-link> –
+        <external-link href="https://www.gnu.org/licenses/gpl-3.0.html">
+          GNU GPLv3
+        </external-link>
         – {{ new Date().getFullYear() }}
       </v-flex>
     </v-layout>
@@ -11,7 +15,12 @@
 </template>
 
 <script>
+import ExternalLink from './AppFooter/ExternalLink';
+
 export default {
   name: 'AppFooter',
+  components: {
+    ExternalLink,
+  },
 };
 </script>
---
+++ src/renderer/components/AppFooter/ExternalLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <a @click="openLink">
+    <slot />
+  </a>
+</template>
+
+<script>
+import { shell } from 'electron';
+
+export default {
+  name: 'ExternalLink',
+  props: {
+    href: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    openLink() {
+      shell.openExternal(this.href);
+    },
+  },
+};
+</script>
