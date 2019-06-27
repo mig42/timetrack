7530c938677d332e63647f603d7c9e84c34ef227
Miguel González <migueljgonz@gmail.com>
2019-06-27 23:32:55 +0200

    Import icon packs using npm packages

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -7,6 +7,10 @@ import db from './datastore';
 import router from './router';
 import store from './store';
 
+/* eslint-disable import/no-extraneous-dependencies */
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import '@mdi/font/css/materialdesignicons.css';
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.db = Vue.prototype.$db = db;
