a577d1c87fc9e679441611db07dd56f2e2c49662
Miguel González <migueljgonz@gmail.com>
2019-06-29 21:23:14 +0200

    Improve layout in controls

--- src/renderer/components/Tracker/TimerControl.vue
+++ src/renderer/components/Tracker/TimerControl.vue
@@ -1,30 +1,38 @@
 <template>
   <v-card class="fill-height">
     <v-card-text>
-      <div class="d-flex row">
-        <v-text-field v-model.number="taskId"
-                      label="TTS ID"
-                      clearable
-                      type="number"
-                      :disabled="running"
-                      @input="$emit('input', taskId)"
-        />
-        <v-btn v-if="!running"
-               color="success"
-               large
-               :disabled="!taskId"
-               @click="start"
-        >
-          Start
-        </v-btn>
-        <v-btn v-if="running"
-               color="error"
-               large
-               @click="stop"
-        >
-          Stop
-        </v-btn>
-      </div>
+      <v-container>
+        <v-layout row>
+          <v-flex grow>
+            <div class="d-flex row">
+              <v-text-field v-model.number="taskId"
+                            label="TTS ID"
+                            clearable
+                            type="number"
+                            :disabled="running"
+                            @input="$emit('input', taskId)"
+              />
+            </div>
+          </v-flex>
+          <v-flex shrink>
+            <v-btn v-if="!running"
+                   color="success"
+                   large
+                   :disabled="!taskId"
+                   @click="start"
+            >
+              Start
+            </v-btn>
+            <v-btn v-if="running"
+                   color="error"
+                   large
+                   @click="stop"
+            >
+              Stop
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-card-text>
   </v-card>
 </template>
