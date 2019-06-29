a0f4e7310c3b96c267d5801619158e17183be0df
Miguel González <migueljgonz@gmail.com>
2019-06-29 20:29:05 +0200

    Move the display functionality to a new control. Have Tracker hold the timer Separate controls and display

--- src/renderer/components/Tracker.vue
+++ src/renderer/components/Tracker.vue
@@ -1,20 +1,54 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <timer />
+      <v-flex xs12 md6>
+        <timer-control v-model="taskId"
+                       :running="timers.updateSeconds.isRunning"
+                       @start="start"
+                       @stop="stop"
+        />
+      </v-flex>
+      <v-flex xs12 md6>
+        <display :elapsed="elapsedSeconds" />
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import Timer from './Tracker/Timer';
+import Display from './Tracker/Display';
+import TimerControl from './Tracker/TimerControl';
 
 export default {
   name: 'Tracker',
   components: {
-    Timer,
+    TimerControl,
+    Display,
+  },
+  data() {
+    return {
+      taskId: 0,
+      elapsedSeconds: 0,
+    };
+  },
+  timers: {
+    updateSeconds: {
+      time: 1000,
+      autostart: false,
+      repeat: true,
+    },
+  },
+  methods: {
+    start() {
+      this.$timer.start('updateSeconds');
+    },
+    stop() {
+      this.$timer.stop('updateSeconds');
+      this.elapsedSeconds = 0;
+    },
+    updateSeconds() {
+      this.elapsedSeconds += 1;
+    },
   },
 };
 </script>
---
+++ src/renderer/components/Tracker/Display.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card flat tile>
+    {{ hours }} : {{ minutes }} : {{ seconds }}
+  </v-card>
+</template>
+
+<script>
+function padDigits(number) {
+  return `${number}`.padStart(2, '0');
+}
+
+export default {
+  name: 'TimerControl',
+  props: {
+    elapsed: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    hours() {
+      return padDigits(Math.floor(this.elapsed / 3600));
+    },
+    minutes() {
+      return padDigits(Math.floor(this.elapsed / 60) % 60);
+    },
+    seconds() {
+      return padDigits(this.elapsed % 60);
+    },
+  },
+};
+</script>
