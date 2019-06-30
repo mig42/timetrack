4f638eb3280d83f3c3fec20f7ec3d3dc966c67d8
Miguel González <migueljgonz@gmail.com>
2019-06-30 22:54:38 +0200

    Make the time separators flicker

--- src/renderer/components/Tracker/Display.vue
+++ src/renderer/components/Tracker/Display.vue
@@ -1,18 +1,23 @@
 <template>
   <v-card :color="color">
     <v-card-text class="display-4 font-weight-bold text-xs-center">
-      {{ hours }}:{{ minutes }}:{{ seconds }}
+      {{ hours }}<ticker :is-on="showDots" />{{ minutes }}<ticker :is-on="showDots" />{{ seconds }}
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import Ticker from './Display/Ticker';
+
 function padDigits(number) {
   return `${number}`.padStart(2, '0');
 }
 
 export default {
   name: 'Display',
+  components: {
+    Ticker,
+  },
   props: {
     elapsed: {
       type: Number,
@@ -35,6 +40,9 @@ export default {
     },
     color() {
       return this.running ? 'accent' : '';
+    },
+    showDots() {
+      return !this.running || this.elapsed % 2 === 1;
     },
   },
 };
---
+++ src/renderer/components/Tracker/Display/Ticker.vue
@@ -0,0 +1,20 @@
+<template>
+  <span :class="{ hidden: !isOn }">:</span>
+</template>
+
+<script>
+export default {
+  name: 'Ticker',
+  props: {
+    isOn: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.hidden
+  visibility hidden
+</style>
