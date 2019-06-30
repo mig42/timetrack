f444bf9dac22adcd5cc7bedddb66ecc3f4e5aefe
Miguel González <migueljgonz@gmail.com>
2019-06-30 23:03:30 +0200

    Make the toolbar more appealing

--- src/renderer/components/Toolbar.vue
+++ src/renderer/components/Toolbar.vue
@@ -1,6 +1,15 @@
 <template>
   <v-toolbar app color="primary">
-    <v-toolbar-title>TimeTrack</v-toolbar-title>
+    <v-toolbar-title>
+      <div class="d-flex align-center">
+        <v-icon x-large>
+          alarm
+        </v-icon>
+        <h1 class="display-1 font-weight-bold font-italic pr-2">
+          TimeTrack
+        </h1>
+      </div>
+    </v-toolbar-title>
   </v-toolbar>
 </template>
 
