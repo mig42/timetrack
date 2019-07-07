de90883cc948baac0153a3f730e83843d0a98b05
Miguel González <migueljgonz@gmail.com>
2019-07-07 21:12:39 +0200

    Add a dialog to enter credentials later

---
+++ src/renderer/components/LoginDialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600">
+    <v-card>
+      <v-card-title class="headline">
+        Log in to TTS
+      </v-card-title>
+      <v-card-text>
+        Please enter your credentials to start tracking your time.
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="green darken-1" flat @click="login">
+          Log in
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'LoginDialog',
+  data() {
+    return {
+      dialog: true,
+    };
+  },
+  methods: {
+    login() {
+      // TODO
+    },
+  },
+};
+</script>
