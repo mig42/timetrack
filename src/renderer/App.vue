46108f8201d27e80a7c8b3981caf09167213263f
Miguel González <migueljgonz@gmail.com>
2019-07-08 21:20:51 +0200

    Add a drawer to contain the log in/out actions

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app id="root">
+    <drawer />
     <toolbar />
     <v-content>
       <v-container fluid>
         <v-layout row justify-center>
-          <login-dialog v-if="!isLoggedIn" />
           <v-flex xs12>
             <router-view />
           </v-flex>
@@ -16,20 +16,16 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
 import AppFooter from './components/AppFooter';
-import LoginDialog from './components/LoginDialog';
+import Drawer from './components/Drawer';
 import Toolbar from './components/Toolbar';
 
 export default {
   name: 'Timetrack',
   components: {
     AppFooter,
-    LoginDialog,
+    Drawer,
     Toolbar,
-  },
-  computed: {
-    ...mapGetters('auth', ['isLoggedIn']),
   },
 };
 </script>
---
+++ src/renderer/components/Drawer.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-navigation-drawer app temporary :permanent="!isLoggedIn">
+    <v-toolbar flat class="transparent">
+      <v-list class="pa-0">
+        <v-list-tile avatar>
+          <v-list-tile-avatar v-if="isLoggedIn">
+            <img :src="`https://ui-avatars.com/api/?name=${encodeURI(user)}`">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title v-if="isLoggedIn">
+              {{ user }}
+            </v-list-tile-title>
+            <v-list-tile-title v-else>
+              Welcome!
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+
+    <v-list class="pt-0" dense>
+      <v-divider />
+
+      <v-list-tile v-if="!isLoggedIn">
+        <v-list-tile-action>
+          <v-icon>lock</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>Log in</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile v-else>
+        <v-list-tile-action>
+          <v-icon>lock_open</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>Log out</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Timetrack',
+  computed: {
+    ...mapGetters('auth', ['isLoggedIn', 'user']),
+  },
+};
+</script>
